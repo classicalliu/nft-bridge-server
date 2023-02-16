811f8e14469293da2ff82a2a41bb26bdbe71126f
classicalliu <classicalliu@gmail.com>
2023-02-16 23:13:51 +0800

    test: Using chai for tests

--- packages/bridge/tests/utils.test.ts
+++ packages/bridge/tests/utils.test.ts
@@ -1,19 +1,20 @@
-import test from "node:test";
-import assert from "node:assert/strict";
+import { assert } from "chai";
 
 import { parseFactoryData } from "../src/base/utils";
 
-test("parseFactoryData", () => {
-  const data =
-    "0x24ff5a9ab8c38d195ce2b4ea75ca8987000854657374204e46540002544e0014687474703a2f2f74657374746f6b656e2e636f6d";
-  const expectedResult = {
-    name: "Test NFT",
-    symbol: "TN",
-    baseUri: "http://testtoken.com",
-    extraData: undefined,
-  };
+describe("utils", () => {
+  it("parseFactoryData", () => {
+    const data =
+      "0x24ff5a9ab8c38d195ce2b4ea75ca8987000854657374204e46540002544e0014687474703a2f2f74657374746f6b656e2e636f6d";
+    const expectedResult = {
+      name: "Test NFT",
+      symbol: "TN",
+      baseUri: "http://testtoken.com",
+      extraData: undefined,
+    };
 
-  const result = parseFactoryData(data);
+    const result = parseFactoryData(data);
 
-  assert.deepStrictEqual(result, expectedResult);
+    assert.deepStrictEqual(result, expectedResult);
+  });
 });
